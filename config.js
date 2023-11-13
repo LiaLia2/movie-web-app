b2d352c9095ef489894f80d25ec47b1239e45957
LiaLia2 <150698803+LiaLia2@users.noreply.github.com>
2023-11-13 12:46:44 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://simple-proxy.rejane.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYWZiYjIyODYyN2UxNWZmY2U1NTVhZWQwNGI4YzA0MiIsInN1YiI6IjY1NTIzYjJlMDgxNmM3MDBjM2Q5ZTc5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-5AHHFqTojri8aeN1O_EkunDuRCtXRNMuvlJLAN3nug"
+};
